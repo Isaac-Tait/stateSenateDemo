580e9475cf028de96d5879c79a0f8125c24f3018
Isaac Tait <isaac@mountaintopcoding.com>
2024-01-24 14:46:32 -0500

    chore: working on google sheet integration 🏉

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import useGoogleSheets from 'use-google-sheets';
 import './App.css';
 
 function App() {
+  const { data, loading, error, refetch } = useGoogleSheets({
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error!</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{JSON.stringify(data)}</div>;
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 }
